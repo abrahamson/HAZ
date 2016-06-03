abrahamson/HAZ

commit c6e88873d962dcf32db6e29f3ac2235b672e8f1a

fixed hypodepth for source type 4

HAZ files/HAZ_MAIN2.f
@@ -41,7 +41,7 @@ c     Probabilisitic Seismic Hazard Program (PSHA)
 c     Write Program information to the screen.
       write (*,*) '*********************************'
       write (*,*) '*   Hazard Code: Version 45.2a  *'
-      write (*,*) '*         May, 2016             *'
+      write (*,*) '*           May, 2016           *'
       write (*,*) '*********************************'
       write (*,*)
 
@@ -320,7 +320,7 @@ c            Integrate Over Rupture Location - Down Dip (aleatory)
 c            Find the Closest Distances for this rupture
 c            Pass along fault grid locations for calculation of HW and Rx values within CalcDist subroutine.     
              call CalcDist (sourceType(iFlt), pscorflag, nFltGrid, n1AS, iLocX, iLocY, n2AS,
-     1             iFltWidth, iFlt, ystep(iFlt), grid_top(iFlt,1), RupWidth, RupLen, r_horiz, mindepth(iFlt), 
+     1             iFltWidth, iFlt, ystep(iFlt), grid_top(iFlt,iLocX), RupWidth, RupLen, r_horiz, mindepth(iFlt), 
      2             fltGrid_x, fltGrid_y, fltGrid_z, fltgrid_x1, fltgrid_y1, fltgrid_z1, fltgrid_x2, 
      3             fltgrid_y2, fltgrid_z2, fltgrid_x3, fltgrid_y3, fltgrid_x4, fltgrid_y4, fltgrid_z4, 
      4             fltGrid_Rrup, fltGrid_Rjb, dip, HWFlag, n1, n2, icellRupstrike, icellRupdip,

commit 7ddf35374ab225e26ff53e53e6ce1d9613847a2c

fixing error introduced in commit c6e8887 that impacted ST 2 and 3

HAZ files/CLDIST.F
@@ -17,7 +17,7 @@ c     (e.g. Rjb, Rrup, ZTOR, Rx, Ry, Ry0)
 c     declarations passed in 
       integer sourceType, pscorflag, nfltGrid(2), n1AS(MAXFLT_AS), iLocX, 
      1        iLocY, n2AS(MAXFLT_AS), iFltWidth, iFlt
-      real ystep, grid_top, RupWidth, RupLen, r_horiz, seismoDepth,
+      real ystep, grid_top(MAX_FLT,MAX_GRID), RupWidth, RupLen, r_horiz, seismoDepth,
      1     fltgrid_x(MAXFLT_DD,MAXFLT_AS), fltgrid_y(MAXFLT_DD,MAXFLT_AS),
      2     fltgrid_z(MAXFLT_DD,MAXFLT_AS), fltgrid_x1(MAXFLT_DD,MAXFLT_AS), 
      3     fltgrid_y1(MAXFLT_DD,MAXFLT_AS), fltgrid_z1(MAXFLT_DD,MAXFLT_AS), 
@@ -34,7 +34,7 @@ c     declarations passed out
 
 c     declarations only used within subroutine
       integer i, j, celly, cellx
-      real hypo1, distRx, rupLength2, r_horiz1, testy, testx, hypox, hypoy 
+      real top_grid, hypo1, distRx, rupLength2, r_horiz1, testy, testx, hypox, hypoy 
       
 c     Initialize closest distance      
       distRup = 1.0e30
@@ -44,11 +44,18 @@ c     Initialize closest distance
       distepi = 1.0e30
       distRx = 1.0e30
       
+c     Set top_grid for sourceTypes 2/3 and 4 (different)
+        if (sourceType .eq. 2 .or. sourceType .eq. 3) then
+          top_grid = grid_top(iFlt,1)
+        else if (sourceType .eq. 4) then
+          top_grid = grid_top(iFlt,iLocX)
+        endif
+      
 c     Compute distances for areal sources      
         if ( sourceType .eq. 2 .or. sourceType .eq. 3 .or. sourceType .eq. 4 ) then
 
 c         Set hypoDepth for areal sources
-          hypoDepth = (iLocY-0.5)*ystep + grid_top
+          hypoDepth = (iLocY-0.5)*ystep + top_grid
  
 c         Approximate correction for using point source with extended source model
 C         added limit on only shallow areal sources (Hypo>30.0km)

HAZ files/HAZ_MAIN2.f
@@ -306,7 +306,7 @@ c            Integrate Over Rupture Location - Down Dip (aleatory)
 c            Find the Closest Distances for this rupture
 c            Pass along fault grid locations for calculation of HW and Rx values within CalcDist subroutine.     
              call CalcDist (sourceType(iFlt), pscorflag, nFltGrid, n1AS, iLocX, iLocY, n2AS,
-     1             iFltWidth, iFlt, ystep(iFlt), grid_top(iFlt,iLocX), RupWidth, RupLen, r_horiz, mindepth(iFlt), 
+     1             iFltWidth, iFlt, ystep(iFlt), grid_top, RupWidth, RupLen, r_horiz, mindepth(iFlt), 
      2             fltGrid_x, fltGrid_y, fltGrid_z, fltgrid_x1, fltgrid_y1, fltgrid_z1, fltgrid_x2, 
      3             fltgrid_y2, fltgrid_z2, fltgrid_x3, fltgrid_y3, fltgrid_x4, fltgrid_y4, fltgrid_z4, 
      4             fltGrid_Rrup, fltGrid_Rjb, dip, HWFlag, n1, n2, icellRupstrike, icellRupdip,